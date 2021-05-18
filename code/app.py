174356c1f393529d5b7ddb5a3b837ff5dbf3f3a5
hjt523 <harry.tindale1997@gmail.com>
2021-05-18 17:01:45 +0100

    Added a fleshed out index page and began work on the Current tasks

--- code/app.py
+++ code/app.py
@@ -13,16 +13,17 @@ db = SQLAlchemy(app) # create SQLALchemy object
 @app.route("/") # The first page people will see on my project
 def index():
 
-    return ""
+    return render_template("index.html")
 
-@app.route("current_projects") # A list of ongoing 3d Prints
-def projects():
+@app.route("/current_projects") # A list of ongoing 3d Prints
+def current_projects():
 
     return ""
-@app.route("add_project") # Add a new 3d print
+@app.route("/add_project") # Add a new 3d print
 def add_project():
 
     return ""
+'''
 @app.route("add_task") # Add a new task for a print / several prints
 def add_task():
     return ""
@@ -32,7 +33,7 @@ def up_project():
 @app.route("update_task") # Update existing tasks
 def up_task():
     return ""
-
+'''
 # My Tables
 
 class project(db.Model):
@@ -45,13 +46,23 @@ class task(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String(30), nullable = False)
     details = db.Column(db.String(60), nullable = True)
-    progress = db.Column(db.String(30), nullable = True) # Turn into drop down options?
+    progress = db.Column(db.String(30), nullable = True, default = "Not Started") # Turn into drop down options?
     complete = db.Column(db.Boolean, nullable=False, default=False)
 
 class jointable(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    project_id = db.Column(db.integer, nullable = False)
-    task_id = db.Column(db.integer,nullable = False)
+    project_id = db.Column(db.Integer, nullable = False)
+    task_id = db.Column(db.Integer,nullable = False)
+
+class projadd(FlaskForm):
+    name = StringField("Project Name")
+    description = StringField("Project Description ( optional)")
+    submit = SubmitField("Submit")
+
+class taskadd(FlaskForm):
+    name = StringField("Task")
+    details = StringField("Project Details ( optional)")
+    submit = SubmitField("Submit")
 
 if __name__=='__main__':
     app.run(debug=True)
